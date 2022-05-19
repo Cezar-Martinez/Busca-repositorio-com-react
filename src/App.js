43836fa448e39770bdbf4e18b9fd3d405cd95f57
Cezar <cezar.martinez@hotmail.com>
2022-05-19 18:48:00 -0300

    adicionando projeto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+/*//import logo from './logo.svg';
+//import './App.css';
+import React, { useState} from "react";
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+function App(props) {
+
+  const [ usuario, setusuario ] = useState('');
+  
+    function handlePesquisa() {
+      axios.get('https://api.github.com/users/${usuario}/repos').then(response => console.log(response.data)) ;
+    }
+return (
+
+    <>    
+
+    <input className="usuarioImput" placeholder="usuário" value={usuario} onChange={e => console.log(e.target.value)} /> <br></br>
+    <button type="button" onClick={handlePesquisa}>Pesquisar</button>
+    </>
   );
 }
 
-export default App;
+export default App;*/
+
+import React from "react";
+import Routes from './routes';
+
+function App() {
+  return (
+    <Routes />
+  );
+}
---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import React from 'React';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+
+import Repositories from './Repositories';
+import Home from './Home';
+
+export default function Routes() {
+    return (
+        <BrowserRouter>
+            <switch>
+                <Route path='/' exact component={Home} />
+                <Route path='/repositories' component={Repositories} />
+            </switch>
+        </BrowserRouter>
+    )
+}
